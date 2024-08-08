27080c918b8076a76bfd15c263cff26c9a3a3966
Clara Picazo <clara.picazo5@gmail.com>
2024-08-08 06:15:28 +0200

    feat: add styles for EditMessageModal

--- frontend/src/components/modals/EditMessageModal/EditMessageModal.tsx
+++ frontend/src/components/modals/EditMessageModal/EditMessageModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Modal from 'react-modal';
 import './editMessageModal.scss';
 
@@ -10,33 +10,58 @@ interface EditMessageModalProps {
   handleSaveMessage: () => void;
 }
 
-const EditMessageModal: React.FC<EditMessageModalProps> = ({
-  isOpen,
-  onRequestClose,
-  editableMessage,
-  setEditableMessage,
-  handleSaveMessage,
-}) => {
-  return (
-    <Modal
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
-      contentLabel="Edit Message"
-      ariaHideApp={false} 
-    >
-      <h2>Edit Message</h2>
-      <textarea
-        value={editableMessage}
-        onChange={(e) => setEditableMessage(e.target.value)}
-        rows={10}
-        cols={50}
-      />
-      <div>
-        <button onClick={handleSaveMessage}>Save</button>
-        <button onClick={onRequestClose}>Cancel</button>
-      </div>
-    </Modal>
-  );
-};
+interface EditMessageModalState {
+  localEditableMessage: string;
+}
+
+class EditMessageModal extends Component<EditMessageModalProps, EditMessageModalState> {
+  constructor(props: EditMessageModalProps) {
+    super(props);
+    this.state = {
+      localEditableMessage: props.editableMessage,
+    };
+  }
+
+  componentDidUpdate(prevProps: EditMessageModalProps) {
+    if (prevProps.editableMessage !== this.props.editableMessage) {
+      this.setState({ localEditableMessage: this.props.editableMessage });
+    }
+  }
+
+  handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    this.setState({ localEditableMessage: event.target.value });
+    this.props.setEditableMessage(event.target.value);
+  };
+
+  handleSave = () => {
+    this.props.handleSaveMessage();
+  };
+
+  render() {
+    const { isOpen, onRequestClose } = this.props;
+    const { localEditableMessage } = this.state;
+
+    return (
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={onRequestClose}
+        contentLabel="Edit Message"
+        ariaHideApp={false}
+      >
+        <h2>Edit Message</h2>
+        <textarea
+          value={localEditableMessage}
+          onChange={this.handleChange}
+          rows={10}
+          cols={50}
+        />
+        <div className='edit-action-buttons'>
+          <button className='save-button'onClick={this.handleSave}>Save</button>
+          <button className='delete-button' onClick={onRequestClose}>Cancel</button>
+        </div>
+      </Modal>
+    );
+  }
+}
 
 export default EditMessageModal;
